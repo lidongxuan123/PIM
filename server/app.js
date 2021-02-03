aa9eafe3f55193d01d69f8d8c9a1db1ee27e3ed8
Dongxuan Li (李东轩)-云数据中心集团 <lidongxuan01@inspur.com>
2021-02-03 09:56:38 +0800

    server

--- server/app.js
+++ server/app.js
@@ -26,6 +26,34 @@ class AppBootHook{
 
     }
     async serverDidReady() {
-        
+        this.app.server.on("upgrade",(req,socket, head)=> {
+            let target = null
+            if(req.url.indexOf("xxx")>-1){
+                target = "url/xxxx"
+            }else {
+                target = "xxxxxx"
+            }
+
+            proxy.ws(req,socket, head, {
+                target: target,
+                ws: true,
+                changeOrigin: true,
+                secure: false,
+                httpVersion: "1.1",
+                xfwd: true,
+                hostRewrite: true,
+                autoRewrite: true,
+                protocolRewrite: 'http',
+                rejectUnauthorized: false,
+                down: false,
+            })
+
+            proxy.on("error",function(err,req,socket) {
+                console.log("xxxxx")
+            })
+        })
+        this.app.messenger.on("checkUser", async options => {
+            const ctx = await this.app.createAnonymouseContent()
+        })
     }
 }
